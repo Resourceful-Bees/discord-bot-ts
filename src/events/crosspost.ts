51410c13c9526cfad1ac016dfbfd12691abeb114
James Moore <th3darkrose@gmail.com>
2023-05-03 15:24:44 -0400

    change event type to messageCreate

--- src/events/crosspost.ts
+++ src/events/crosspost.ts
@@ -1,16 +1,13 @@
-import { Client, ChannelType } from "discord.js";
-import {DiscordEvent} from "../utils/types";
+import { ChannelType, Message } from "discord.js";
+import { DiscordEvent } from "../utils/types";
 
 export default {
-    name: "ready",
-    once: true,
-    execute(client: Client) {
-        console.log(`Activating auto-publish of announcement channels!`);
-
-        client.on('message', (message) => {
-            if (message.channel.type === ChannelType.GuildAnnouncement && message.author.bot) {
-                message.crosspost()
-            }
-        })
+    name: "messageCreate",
+    execute(message: Message) {
+        if (message.channel.type === ChannelType.GuildAnnouncement && message.author.bot) {
+            message.crosspost()
+                .then(() => console.log("crossposted message!"))
+                .catch(console.error);
+        }
     }
 } as DiscordEvent;
