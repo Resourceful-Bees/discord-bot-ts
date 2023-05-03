625e7726de37cd09955c5adef88db8e7f6d77f08
James Moore <th3darkrose@gmail.com>
2023-05-03 15:31:40 -0400

    add some extra loggers for testing

--- src/events/crosspost.ts
+++ src/events/crosspost.ts
@@ -4,7 +4,11 @@ import { DiscordEvent } from "../utils/types";
 export default {
     name: "messageCreate",
     execute(message: Message) {
+        console.log(`testing message for crosspost:`);
+        console.log(`type: ${message.channel.type}`);
+        console.log(`isBot: ${message.author.bot}`);
         if (message.channel.type === ChannelType.GuildAnnouncement && message.author.bot) {
+            console.log("crossposting message");
             message.crosspost()
                 .then(() => console.log("crossposted message!"))
                 .catch(console.error);
