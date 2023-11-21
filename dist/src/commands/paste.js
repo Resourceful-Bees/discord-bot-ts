79b88f54b8e656ba5d4b91ff07a624c62dd99ee2
James Moore <th3darkrose@gmail.com>
2023-11-21 14:22:04 -0500

    update workflow steps

--- dist/src/commands/paste.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    id: "paste",
-    execute(command) {
-        const file = command.options.getAttachment("file", true);
-    }
-};
-//# sourceMappingURL=paste.js.map
