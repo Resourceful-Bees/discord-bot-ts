073eeb7b1e1ae50155011daff3f76fd432cd3234
James Moore <th3darkrose@gmail.com>
2023-11-21 14:45:56 -0500

    change process import

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@
 import {Client, GatewayIntentBits} from "discord.js";
 import { Interactions } from "./utils/interactions";
 import {Commands} from "./utils/commands";
-import * as process from "process";
+import process from "node:process";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages] });
 const interactions = new Interactions(client);
 const commands = new Commands();
-
 const getCommands = () => commands;
 const getInteractions = () => interactions;
 
